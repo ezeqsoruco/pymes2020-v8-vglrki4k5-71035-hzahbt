037e2ad851bd45d7f9f1f1ddd49bd893d831ad0a
ezeqsoruco <eze.soruco93@gmail.com>
2020-10-26 17:33:28 -0300

    3 files modified

--- src/app/components/servicios/servicios.component.ts
+++ src/app/components/servicios/servicios.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ServiciosService } from "../../services/servicios.service";
 import { Servicio } from "../../models/servicio";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-servicios",
@@ -17,10 +18,39 @@ export class ServiciosComponent implements OnInit {
   Items: Servicio[] = [];
 
   AccionABMC = "L";
-  constructor(private serviciosService: ServiciosService) {}
+
+  FormReg: FormGroup;
+  constructor(
+    private serviciosService: ServiciosService,
+    public formBuilder: FormBuilder
+  ) {}
 
   ngOnInit() {
     this.GetServicios();
+    this.FormReg = this.formBuilder.group({
+      idservicio: [0],
+      Descripcion: [
+        "",
+        [Validators.required, Validators.minLength(4), Validators.maxLength(55)]
+      ],
+      Precio: [null, [Validators.required, Validators.pattern("[0-9]{1,7}")]],
+      Stock: [null, [Validators.required, Validators.pattern("[0-9]{1,7}")]],
+      CodigoDeBarra: [
+        "",
+        [Validators.required, Validators.pattern("[0-9]{13}")]
+      ],
+      IdArticuloFamilia: ["", [Validators.required]],
+      FechaAlta: [
+        "",
+        [
+          Validators.required,
+          Validators.pattern(
+            "(0[1-9]|[12][0-9]|3[01])[-/](0[1-9]|1[012])[-/](19|20)[0-9]{2}"
+          )
+        ]
+      ],
+      Activo: [true]
+    });
   }
 
   GetServicios() {
@@ -31,5 +61,9 @@ export class ServiciosComponent implements OnInit {
 
   Agregar() {
     this.AccionABMC = "A";
+  }
+
+  Volver() {
+    this.AccionABMC = "L";
   }
 }
