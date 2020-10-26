7539bbc8e02d7da229233c215e002678cbacb353
ezeqsoruco <eze.soruco93@gmail.com>
2020-10-26 16:13:32 -0300

    1 file modified, 1 added

--- src/app/components/servicios/servicios.component.ts
+++ src/app/components/servicios/servicios.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-servicios',
-  templateUrl: './servicios.component.html',
-  styleUrls: ['./servicios.component.css']
+  selector: "app-servicios",
+  templateUrl: "./servicios.component.html",
+  styleUrls: ["./servicios.component.css"]
 })
 export class ServiciosComponent implements OnInit {
+  Titulo = "Servicios";
+  TituloAccionABMC = {
+    A: "(Agregar)",
+    L: "(Listado)"
+  };
 
-  constructor() { }
+  AccionABMC = "L";
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
---
+++ src/app/models/servicio.ts
@@ -0,0 +1,6 @@
+export class Servicio {
+  idServicio: number;
+  descripcion: string;
+  importe: number;
+  cantidadHoras: number;
+}
