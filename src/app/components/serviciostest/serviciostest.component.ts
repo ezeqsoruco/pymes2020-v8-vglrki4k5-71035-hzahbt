605b84dabb37103a089c6c29abfc9c7bb989a92b
ezeqsoruco <eze.soruco93@gmail.com>
2020-10-26 15:43:27 -0300

    1 file modified, 2 added

---
+++ src/app/components/serviciostest/serviciostest.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-serviciostest',
+  templateUrl: './serviciostest.component.html',
+  styleUrls: ['./serviciostest.component.css']
+})
+export class ServiciostestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
